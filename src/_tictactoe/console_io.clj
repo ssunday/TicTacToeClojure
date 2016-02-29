e78c7d0a216664c78c1741483d8f0328c0cbafab
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-02-29 13:29:50 -0600

    More bad-input catching.
    Basically have the loop running as it should. Next is to add AI/Human
    options instead of only being human vs human.

--- src/_tictactoe/console_io.clj
+++ src/_tictactoe/console_io.clj
@@ -6,43 +6,65 @@
 (defn end-game-message []
   (println "Thank you for playing!"))
 
-(defn convert_string_to_number [str]
+(defn convert-string-to-number [str]
   (let [spot (read-string str)]
        (if (number? spot) spot nil)))
 
 (defn get-player-spot-to-be-marked [board]
   (println "Please input spot to be marked. Must be 0-8 and available.")
-  (loop [spot (convert_string_to_number (read-line))
+  (loop [spot (convert-string-to-number (read-line))
         board board]
-    (if (nil? (find board spot))
+    (if (or (nil? (find board spot))
+            (not  (number? (get board spot))))
       (do
         (println "Spot must be open.")
-        (recur (convert_string_to_number (read-line)) board))
-      spot)
-  )
+        (recur (convert-string-to-number (read-line)) board))
+      spot))
 )
 
-
 (defn display-game-board [board]
+  (println)
   (apply println (subvec board 0 3))
   (apply println (subvec board 3 6))
   (apply println (subvec board 6 9))
+  (println)
 )
 
 (defn get-player-one-marker []
   (println "Please input player one's marker. Marker must be a single character long and not a number.")
-  (read-line)
-)
+  (loop [player-one-marker (read-line)]
+    (if (or (> (count player-one-marker) 1)
+            (not (nil? (convert-string-to-number player-one-marker))))
+        (do
+          (println "Marker must be a single character and not a number.")
+          (recur (read-line)))
+        (clojure.string/upper-case player-one-marker))))
 
 (defn get-player-two-marker [player-one-marker]
   (println "Please input player two's marker. Marker must be a single character long, not a number, and different from player one's marker.")
   (loop [player-two-marker (read-line)
         player-one-marker player-one-marker]
-    (if (= player-two-marker player-one-marker)
+    (if (or (= player-two-marker player-one-marker)
+             (> (count player-two-marker) 1)
+             (not (nil? (convert-string-to-number player-one-marker))))
         (do
-          (println "Marker must be unique.")
+          (println "Marker must be unique, single length, and not a number.")
           (recur (read-line) player-one-marker))
-        player-two-marker)))
+        (clojure.string/upper-case player-two-marker))))
+
+(defn get-first-player [player-one-marker player-two-marker]
+  (println "Please input marker of playing going first. Either:" player-one-marker "or" player-two-marker)
+  (loop [first-player-marker (read-line)]
+    (if (and (not (= first-player-marker player-one-marker))
+             (not (= first-player-marker player-two-marker)))
+             (do
+              (println "Please input one of the already defined markers. Either:" player-one-marker "or" player-two-marker)
+              (recur (read-line)))
+              first-player-marker))
+)
+
+(defn display-current-player-marker [current-player-marker]
+  (println "\nCurrent Player Marker:" current-player-marker))
 
 (defn player-one-won-message []
   (println "Player One Won!"))
--- src/_tictactoe/core.clj
+++ src/_tictactoe/core.clj
@@ -8,17 +8,19 @@
           (io/player-two-won-message)
           (io/player-one-won-message))
       (io/game-is-tied-message)))
-      
+
 (defn -main []
   (io/start-game-message)
   (let [player-one-marker (io/get-player-one-marker)
         player-two-marker (io/get-player-two-marker player-one-marker)
+        first-player (io/get-first-player player-one-marker player-two-marker)
         board (gf/make-default-board)]
-    (loop [player-marker player-one-marker
-          other-player-marker player-two-marker
+    (loop [player-marker first-player
+          other-player-marker (if (= first-player player-one-marker) player-two-marker player-one-marker)
           board board]
         (if (not (gf/game-is-won-or-tied board))
-            (do (io/display-game-board board)
+            (do (io/display-current-player-marker player-marker)
+                (io/display-game-board board)
                 (recur other-player-marker player-marker
                       (gf/mark-board-location board (io/get-player-spot-to-be-marked board) player-marker)))
             (display-end-game-messages board player-marker player-one-marker))))
