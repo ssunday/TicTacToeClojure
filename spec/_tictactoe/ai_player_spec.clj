1cc8de338b250b4099b9b41eb9a936f4a91c7291
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-03-01 11:05:34 -0600

    First draft of the minimax algorithm
    Not sure if it works yet, going to begin testing it out.

--- spec/_tictactoe/ai_player_spec.clj
+++ spec/_tictactoe/ai_player_spec.clj
@@ -4,5 +4,11 @@
 
 (describe "get-available-locations"
   (it "returns only numbered, open spots"
-    (should= (get-available-locations [0 "X" "O" 3 4 5 "X" "X" "O"]) '(0 3 4 5)))
+    (should= (get-available-locations [0 "X" "O" 3 4 5 "X" "X" "O"]) [0 3 4 5]))
+)
+
+
+(describe "best-move"
+  (it "returns corner spot"
+    (should= (best-move [0 1 2 3 4 5 6 7 8] "X" "O") 8))
 )
--- src/_tictactoe/ai_player.clj
+++ src/_tictactoe/ai_player.clj
@@ -2,9 +2,30 @@
   (:require [-tictactoe.game_functions :as gf]))
 
 (defn get-available-locations [board]
-  (filter number? board))
+  (vec (filter number? board)))
 
-(defn move
+(defn score [board current-player player-marker depth]
+  (cond (and (gf/game-is-won board)
+              (= current-player player-marker)) (- 10 depth)
+        (and (gf/game-is-won board)
+                    (not (= current-player player-marker))) (- depth 10)
+        (gf/game-is-tied board) 0)
+)
+
+(defn best-move
   [board player-marker other-player-marker]
-
+  (let [best-moves (atom (zipmap (get-available-locations board) [0 0 0 0 0 0 0 0 0]))]
+  (loop [board board
+         current-player player-marker
+         other-player other-player-marker
+         depth 0
+         previous-move nil]
+        (if (gf/game-is-won-or-tied board)
+            (swap! best-moves assoc previous-move (score board current-player player-marker depth))
+            (recur (gf/mark-board-location board (first (get-available-locations board)) current-player)
+            other-player
+            current-player
+            (inc depth)
+            (first (get-available-locations board)))))
+  (key (apply max-key val @best-moves)))
 )
