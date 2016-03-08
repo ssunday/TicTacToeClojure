13dca88aef2ebfd44644dddd1bb2ade57e14deab
Sarah <sarahsunday@Sarahs-MacBook-Pro.local>
2016-03-08 08:40:00 -0600

    Game can be played in either english or german.
    Controlled by environment variable that is passed in in the command
    line. English by default.

--- src/_tictactoe/game_menu.clj
+++ src/_tictactoe/game_menu.clj
@@ -1,15 +1,14 @@
 (ns -tictactoe.game_menu
   (:require [-tictactoe.display_previous_scores :refer :all]
-            [-tictactoe.play_game :refer :all]))
+            [-tictactoe.play_game :refer :all]
+            [-tictactoe.localization :refer :all]))
 
-(def game_menu {1 "Play Game"
-                2 "See Scores"
-                3 "End Application"})
+(def game_menu {1 "play-game"
+                2 "see-scores"
+                3 "end-application"})
 
 (defn get-menu-options []
   game_menu)
-
-
 
 (defn do_menu_option [option]
   (cond (= option 1) (run-game)
--- src/_tictactoe/localization.clj
+++ src/_tictactoe/localization.clj
@@ -2,13 +2,77 @@
   (:require [taoensso.tower :as tower :refer (with-tscope)]))
 
 (def t-config
-  {:dictionary {:en {:output {:welcome-message "Welcome to the Tic Tac Toe Game!"
+  {:dictionary {:en {:input {:spot-to-be-marked "Please input spot to be marked. Must be 0 - %s and open."
+                             :player-one-name "Please input player one's name."
+                             :player-two-name "Please input player two's name."
+                            :player-one-marker "Please input player one's marker. Marker must be a single character long and not a number."
+                            :player-two-marker "Please input player two's marker. Marker must be a single character long, not a number, and different from player one's marker."
+                            :player-one-ai "Input either y/n for player one being an AI."
+                            :player-two-ai "Input either y/n for player two being an AI."
+                            :player-going-first "Please input marker of player going first. Either: %1$s or %2$s."
+                            :board-dimension "Choose either 3x3 or 4x4 board. Type 3 for 3x3 and 4 for 4x4."
+                            :play-again-same-input "Play again with same input? (y/n)"
+                            :play-again-new-input "Play again with new input? (y/n)"}
+                    :output {:welcome-message "Welcome to the Tic Tac Toe Game!"
                               :current-player-marker "Current Player Marker: %s"
+                              :player-scores "Player Scores:"
+                              :player-one-has-won "Player One has Won!"
+                              :player-two-has-won "Player Two has Won!"
+                              :game-has-been-tied "Tied!"
                               :end-game-message "Thank you for playing!"}
+                    :error-messages {:spot-not-open "Spot must be open and unmarked."
+                                    :spot-is-invalid-input "Spot must be an integer corresponding to an open location."
+                                    :spot-is-not-on-board "Spot must be a location on the game board."
+                                    :name-must-not-be-blank "Name must not be blank."
+                                    :name-must-not-be-blank-or-identical "Player names cannot be identical or blank."
+                                    :bad-player-one-marker "Marker must be a single character and not a number."
+                                    :bad-player-two-marker "Marker must be unique, single length, and not a number."
+                                    :invalid-player-going-first "Please input one of the already defined markers. Either: %1$s or %2$s"
+                                    :invalid-board-dimension "Type 3 for 3x3 and 4 for 4x4."
+                                    :invalid-menu-option "Option must be a number matched with one of the available options."
+                                    :bad-y-or-n-option "Please input either y or n."
+                                    }
+                    :menu {:menu "Menu"
+                          :select-menu-option "Please select option by typing in its number."
+                          :play-game "Play Game"
+                          :see-scores "See Scores"
+                          :end-application "End Application"}
                     :missing "Translation missing"}
-                :de {:output {:welcome-message "Willkommen in der Tic Tac Toe-Spiel!"
+                :de {:input {:spot-to-be-marked "Bitte geben Sie Ort und Stelle zu kennzeichnen. Muss 0 bis %s und offen sein."
+                             :player-one-name "Bitte geben erste Spielernamen."
+                             :player-two-name "Bitte geben Sie Name des zweiten Spielers."
+                             :player-one-marker "Bitte geben Sie Spieler einen der Marker. Marker muss ein einzelnes Zeichen lang und nicht eine Zahl sein."
+                             :player-two-marker "Bitte geben Spieler zwei der Marker. Marker muss ein einzelnes Zeichen lang sein, keine Zahl, und unterscheidet sich von Spieler einen der Markierung."
+                             :player-one-ai "Input entweder y/n für den Spieler eine eine künstliche Intelligenz zu sein."
+                             :player-two-ai "Input entweder y / n für zwei Spieler eine künstliche Intelligenz zu sein."
+                             :player-going-first "Bitte geben Sie Marker der Spieler geht zuerst Entweder: %1$s oder %2$s"
+                             :board-dimension "Wählen Sie entweder 3x3 oder 4x4 Bord. Typ 3 für 3x3 und 4 für 4x4."
+                             :play-again-same-input "Play wieder mit dem gleichen Eingang? (y/n)"
+                             :play-again-new-input "Play wieder mit neuen Eingang? (y/n)"}
+                    :output {:welcome-message "Willkommen in der Tic Tac Toe-Spiel!"
                               :current-player-marker "Aktuelle Spieler Marker: %s"
+                              :player-scores "Spieler Ergebnis"
+                              :player-one-has-won "Erste Spieler hat Gewonnen!"
+                              :player-two-has-won "Zweite Spieler hat Gewonnen!"
+                              :game-has-been-tied "Spiel wurde Gebunden!"
                               :end-game-message "Danke für das Spiel"}
+                    :error-messages {:spot-not-open "Stelle muss offen sein und nicht markierte."
+                                    :spot-is-invalid-input "Punkt muss eine ganze Zahl entsprechend einer offenen Position sein."
+                                    :spot-is-not-on-board "Punkt muss eine Position auf dem Spielbrett sein."
+                                    :name-must-not-be-blank "Der Name darf nicht leer sein."
+                                    :name-must-not-be-blank-or-identical "Spielernamen können nicht identisch oder leer sein."
+                                    :bad-player-one-marker "Marker muss ein einzelnes Zeichen und keine Zahl sein."
+                                    :bad-player-two-marker "Marker muss eindeutig, einzelne Länge sein, und keine Zahl."
+                                    :invalid-player-going-first "Eingang eines der bereits definierten Marker Bitte Entweder: %1$s oder %2$s."
+                                    :invalid-board-dimension "Typ 3 für 3x3 und 4 für 4x4."
+                                    :invalid-menu-option "Option muss eine Zahl mit einer der verfügbaren Optionen angepasst werden."
+                                    :bad-y-or-n-option "Bitte geben Sie entweder y oder n."
+                                    }
+                    :menu {:menu "Menü"
+                            :select-menu-option "Bitte wählen Sie, indem Sie im Eingabe seiner Nummer."
+                            :play-game "Spielen"
+                            :see-scores "Siehe Ergebnisse"
+                            :end-application "End-Anwendung"}
                     :missing "Übersetzung fehlt"}}
                 :fallback-locale :en})
 
